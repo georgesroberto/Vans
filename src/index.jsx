2dfff41bd92bb20fddc32799dd823d3b4ed321ea
georgesroberto <georgesroberto21@gmail.com>
2024-04-13 17:09:50 +0300

    Add miragejs mock server

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import { makeServer } from "./server";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer({ environment: "development" });
+}
 
 function App() {
   return (
     <BrowserRouter>
-      
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
 
